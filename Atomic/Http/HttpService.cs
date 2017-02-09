dc36176214ea80fd3bc94c83c237a835caf40550
Tobias Boogh <tobias.boogh@bybrick.se>
2017-02-10 00:45:02 +0100

    Changed que start mechanism

--- Atomic/Http/HttpService.cs
+++ Atomic/Http/HttpService.cs
@@ -18,54 +18,72 @@ namespace Atomic.Core.Http
         {
             _storage = storage;
             _httpClient = httpClient ?? new HttpClient();
+			_httpClient.Timeout = TimeSpan.FromSeconds(30);
         }
 
         public IObservable<double> DownloadFileAsync(string url, string fileIdentifier)
         {
 			return Observable.Create<double>(async observer => {
 				CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
-				cancellationTokenSource.Token.Register(() => {
-					var x = 0;
-				});
 				
-				HttpResponseMessage response = await _httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead, cancellationTokenSource.Token);
-
+				HttpResponseMessage response = null;
+				try {
+					response = await _httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead, cancellationTokenSource.Token);
+				} catch (Exception e) {
+					observer.OnError(e);
+					return Disposable.Empty;
+				}
+	
 				if (!response.IsSuccessStatusCode) {
 					observer.OnError(new HttpStatusCodeException(response.StatusCode));
+					return Disposable.Empty;
 				}
 
 				var total = response.Content.Headers.ContentLength ?? -1L;
 				var canReportProgress = total != -1;
 				observer.OnNext(0.0);
 				using (var transientStream = _storage.GetTransientOutputStream(fileIdentifier)) {
-					using (var stream = await response.Content.ReadAsStreamAsync()) {
-						long totalRead = 0L;
-						byte[] buffer = new byte[4096];
-						bool isMoreToRead = true;
+					System.IO.Stream stream = null;
+					try {
+						stream = await response.Content.ReadAsStreamAsync();
+					} catch (Exception e){
+						stream.Dispose();
+						observer.OnError(e);
+						return Disposable.Empty;
+					}
+						
+					long totalRead = 0L;
+					byte[] buffer = new byte[4096];
+					bool isMoreToRead = true;
+	
+					do {
+						int readCount = 0;
+						try {
+							readCount = await stream.ReadAsync(buffer, 0, buffer.Length, cancellationTokenSource.Token);
+						} catch(Exception e){
+							observer.OnError(e);
+							break;
+						}
 
-						do {
-							var readCount = await stream.ReadAsync(buffer, 0, buffer.Length, cancellationTokenSource.Token);
-
-							if (readCount == 0) {
-								isMoreToRead = false;
-							} else {
-								byte[] data = new byte[readCount];
-								buffer.ToList()
-									.CopyTo(0, data, 0, readCount);
-
-								transientStream.Write(data, 0, readCount);
-
-								totalRead += readCount;
-
-								if (canReportProgress) {
-									double value = (totalRead / (double)total);
-									observer.OnNext(value);
-								}
+						if (readCount == 0) {
+							isMoreToRead = false;
+						} else {
+							byte[] data = new byte[readCount];
+							buffer.ToList()
+								.CopyTo(0, data, 0, readCount);
+	
+							transientStream.Write(data, 0, readCount);
+	
+							totalRead += readCount;
+	
+							if (canReportProgress) {
+								double value = (totalRead / (double)total);
+								observer.OnNext(value);
 							}
 						}
-						while (isMoreToRead);
-					}
-					transientStream.Flush();
+					} while (isMoreToRead);
+					stream.Dispose();
+					
 					observer.OnNext(1.0);
 					observer.OnCompleted();
 				}
--- Atomic/Managers/IDownloadManager.cs
+++ Atomic/Managers/IDownloadManager.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
@@ -10,7 +12,6 @@ using Atomic.Core.Model;
 
 namespace Atomic.Core.Managers {
 	public interface IDownloadManager {
-		ReadOnlyObservableCollection<IDownload> Downloads { get; }
 		int NumberOfConcurrentDownloads { get; set; }
 		int MaxRetryCount { get; set; }
 
@@ -22,7 +23,7 @@ namespace Atomic.Core.Managers {
     }
 
 	public class DownloadManager : IDownloadManager {
-		private readonly ObservableCollection<IDownload> _downloads;
+		
 		private readonly Subject<int> _progressSubject = new Subject<int>();
         private readonly Subject<IDownload> _downloadUpdateSubject = new Subject<IDownload>();
         private Task _queueTask;
@@ -30,11 +31,11 @@ namespace Atomic.Core.Managers {
 		public IHttpService HttpService { get; }
 		public ConcurrentDictionary<string, Tuple<IDisposable, IDownload>> CurrentDownloadDictionary { get; } = new ConcurrentDictionary<string, Tuple<IDisposable, IDownload>>();
 		public ConcurrentQueue<IDownload> DownloadQueue { get; } = new ConcurrentQueue<IDownload>();
-
+		private List<IDownload> _downloads = new List<IDownload>();
+		
 		public DownloadManager(IHttpService httpService) {
 			HttpService = httpService;
-			_downloads = new ObservableCollection<IDownload>();
-			Downloads = new ReadOnlyObservableCollection<IDownload>(_downloads);
+
 			NumberOfConcurrentDownloads = 4;
 			MaxRetryCount = 3;
 		}
@@ -52,7 +53,6 @@ namespace Atomic.Core.Managers {
 			}
 
 			Download download = new Download(url, assetFilename ?? Guid.NewGuid().ToString());
-			_downloads.Add(download);
             download.Status = DownloadStatus.Queued;
 			DownloadQueue.Enqueue(download);
             _downloadUpdateSubject.OnNext(download);
@@ -62,70 +62,81 @@ namespace Atomic.Core.Managers {
 		}
 
 		public void StartQueue() {
-			if (_queueTask?.IsCompleted ?? true) {
-				_queueTask = ProcessQueue();
-			}
+			//if (_queueTask?.IsCompleted ?? true) {
+				
+			//	_queueTask = ProcessQueue();
+			//}
+			ProcessQueue();
 		}
 
-		public async Task ProcessQueue() {
-			do {
-				IDownload outDownload;
-				if (!DownloadQueue.TryDequeue(out outDownload)) {
-					break;
-				}
-
-				// If the download has been cancelled we skip it
-				if (outDownload.Status == DownloadStatus.Cancelled)
-					continue;
-
-				var download = (Download)outDownload;
-				download.Status = DownloadStatus.InProgress;
-
-				TaskCompletionSource<bool> taskCompletionSource = new TaskCompletionSource<bool>();
-
-				IObservable<double> downloadFileObservable = HttpService.DownloadFileAsync(download.Url, download.FileIdentifier);
-
-				IDisposable disposable = downloadFileObservable.Subscribe(d => {
-					download.ProgressSubject.OnNext(d);
-				}, exception => {
-					download.ErrorCount++;
-
-					if (download.ErrorCount >= 3) {
-						_downloads.Remove(download);
-						download.Status = DownloadStatus.Failed;
-                        _downloadUpdateSubject.OnNext(download);
-
-                        download.ProgressSubject.OnError(exception);
-						_progressSubject.OnNext(_downloads.Count);
-					} else {
-                        // Download failed so we add it to queue again
-                        download.Status = DownloadStatus.Queued;
-                        _downloadUpdateSubject.OnNext(download);
-                        DownloadQueue.Enqueue(download);
-						StartQueue();
-					}
-
-					taskCompletionSource.TrySetResult(true);
-				}, () => {
-					_downloads.Remove(download);
-					download.Status = DownloadStatus.Complete;
-                    _downloadUpdateSubject.OnNext(download);
-                    taskCompletionSource.TrySetResult(true);
-
-					download.ProgressSubject.OnCompleted();
-					_progressSubject.OnNext(_downloads.Count);
-					StartQueue();
-				});
-
-				if (!CurrentDownloadDictionary.TryAdd(download.Url, new Tuple<IDisposable, IDownload>(disposable, download))) {
-					throw new Exception("Could not add download to dictionary");
-				}
-
-				while (Downloads.Count(x => x.Status == DownloadStatus.InProgress) >= NumberOfConcurrentDownloads) {
-					await taskCompletionSource.Task;
-				}
+		public void ProcessQueue() {
+			if (_downloads.Count >= NumberOfConcurrentDownloads)
+				return;
+				
+			IDownload outDownload;
+			if (!DownloadQueue.TryDequeue(out outDownload)) {
+				return;
 			}
-			while (true);
+
+			// If the download has been cancelled we skip it
+			if (outDownload.Status == DownloadStatus.Cancelled)
+				return;
+			
+			var download = (Download)outDownload;
+			_downloads.Add(outDownload);
+			download.Status = DownloadStatus.InProgress;
+			Debug.WriteLine($"Download Start: {download.FileIdentifier}");
+			TaskCompletionSource<bool> taskCompletionSource = new TaskCompletionSource<bool>();
+
+			IObservable<double> downloadFileObservable = HttpService.DownloadFileAsync(download.Url, download.FileIdentifier);
+
+			IDisposable disposable = downloadFileObservable.Subscribe(d => {
+				download.ProgressSubject.OnNext(d);
+			}, exception => {
+				Debug.WriteLine($"Download Error: {download.FileIdentifier}");
+				download.ErrorCount++;
+
+				if (download.ErrorCount >= 3) {
+					Debug.WriteLine($"Download Error Remove: {download.FileIdentifier}");
+					
+					download.Status = DownloadStatus.Failed;
+                    _downloadUpdateSubject.OnNext(download);
+
+                    download.ProgressSubject.OnError(exception);
+					_progressSubject.OnNext(_downloads.Count);
+				} else {
+					Debug.WriteLine($"Download Error Qeueu: {download.FileIdentifier}");
+
+                    // Download failed so we add it to queue again
+                    download.Status = DownloadStatus.Queued;
+                    _downloadUpdateSubject.OnNext(download);
+                    DownloadQueue.Enqueue(download);
+				}
+				_downloads.Remove(download);
+				StartQueue();
+			}, () => {
+				Debug.WriteLine($"Download Complete: {download.FileIdentifier}");
+				_downloads.Remove(download);
+				download.Status = DownloadStatus.Complete;
+                _downloadUpdateSubject.OnNext(download);
+                
+                taskCompletionSource.TrySetResult(true);
+
+				download.ProgressSubject.OnCompleted();
+				_progressSubject.OnNext(_downloads.Count);
+				StartQueue();
+			});
+
+			if (!CurrentDownloadDictionary.TryAdd(download.Url, new Tuple<IDisposable, IDownload>(disposable, download))) {
+				throw new Exception("Could not add download to dictionary");
+			}
+
+			//while (Downloads.Count(x => x.Status == DownloadStatus.InProgress) >= NumberOfConcurrentDownloads) {
+			//	Debug.WriteLine("Start wait");
+			//	await taskCompletionSource.Task;
+			//	Debug.WriteLine("End wait");
+			//}
+			StartQueue();
 		}
 
 		public void CancelDownload(string url) {
